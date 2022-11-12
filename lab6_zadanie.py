2fab992075ac1fce056c485f2c17eae503cdbb2d
Mateusz <mateusz.ruszczyk97@gmail.com>
2022-11-12 23:44:49 +0100

    powstanie

---
+++ lab6_zadanie.py
@@ -0,0 +1,6 @@
+import requests
+
+res = requests.get('https://www.castorama.pl/cataloginventory/index/checkAvailabilityInStores?show_all_markets=true&qty=1&sku=318070')
+
+print(res.text)
+print(res.status_code)
